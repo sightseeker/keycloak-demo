581c808acef5f000f2e0def48b94caf657df9286
SightSeeker <master@SAITO-no-MacBook-Air.local>
2018-04-07 22:08:31 +0900

    initial commit

---
+++ src/main/java/com/sightseekerstudio/demo/App.java
@@ -0,0 +1,13 @@
+package com.sightseekerstudio.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
